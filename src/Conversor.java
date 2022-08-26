c96b67211e086e223ebac2c0e53ab5a7184ab096
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-26 12:08:52 -0300

    conversão corrigida

--- src/Conversor.java
+++ src/Conversor.java
@@ -24,11 +24,12 @@ public class Conversor {
 		
 		if(opcao == 1) {
 			valor = JOptionPane.showInputDialog("Digite o valor em reais que deseja converter para Euros:");
+			
 			double valorReal = Double.parseDouble(valor);
 			
-			final double euro = 0.19;
+			double euro = valorReal / 5.0891;
 			
-			double valorConvertido = valorReal * euro;
+			double valorConvertido = Math.round(euro * 100d) / 100;
 			
 			msg = "O valor em Euro é: " + valorConvertido;
 			
@@ -39,65 +40,107 @@ public class Conversor {
 		
 		else if (opcao == 2) {
 			valor = JOptionPane.showInputDialog("Digite o valor em reais que deseja converter para Dólares:");
+			
 			double valorReal = Double.parseDouble(valor);
-			final double dolar = 0.19;
-			double valorConvertido = valorReal * dolar;
+			
+			double dolar = valorReal / 5.0761;
+			
+			double valorConvertido = Math.round(dolar * 100d) / 100;
+			
 			msg = "O valor em Dólar é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 3) {
 			valor = JOptionPane.showInputDialog("Digite o valor em reais que deseja converter para Peso Argentino:");
+			
 			double valorReal = Double.parseDouble(valor);
-			final double argentino = 26.11;
-			double valorConvertido = valorReal * argentino;
+			
+			double argentino = valorReal / 0.0369;
+			
+			double valorConvertido = Math.round(argentino * 100d) / 100;
+			
 			msg = "O valor em Peso Argentino é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 4) {
 			valor = JOptionPane.showInputDialog("Digite o valor em reais que deseja converter para Peso Chileno:");
+			
 			double valorReal = Double.parseDouble(valor);
-			final double chileno = 175.44;
-			double valorConvertido = valorReal * chileno;
+			
+			double chileno = valorReal / 0.005618;
+			
+			double valorConvertido = Math.round(chileno * 100d) / 100;
+			
 			msg = "O valor em Peso Chileno é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 5) {
 			valor = JOptionPane.showInputDialog("Digite o valor em Euros que deseja converter para Reais:");
+			
 			double valorEuro = Double.parseDouble(valor);
-			final double reais = 5.25;
-			double valorConvertido = valorEuro * reais;
+			
+			double reais = valorEuro * 5.11;
+			
+			double valorConvertido = Math.round(reais * 100d) / 100;
+			
 			msg = "O valor em Reais é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 6) {
 			valor = JOptionPane.showInputDialog("Digite o valor em Dólares que deseja converter para Reais:");
+			
 			double valorDolar = Double.parseDouble(valor);
-			final double reais = 5.19;
-			double valorConvertido = valorDolar * reais;
+			
+			double reais = valorDolar * 5.08;
+			
+			double valorConvertido = Math.round(reais * 100d) / 100;
+			
 			msg = "O valor em Reais é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 7) {
 			valor = JOptionPane.showInputDialog("Digite o valor em Peso Argentino que deseja converter para Reais:");
+			
 			double valorArgentino = Double.parseDouble(valor);
-			final double reais = 0.038;
+			
+			double reais = 0.038;
+			
 			double valorConvertido = valorArgentino * reais;
+			
 			msg = "O valor em Reais é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 		else if (opcao == 8) {
 			valor = JOptionPane.showInputDialog("Digite o valor em Peso Chileno que deseja converter para Reais:");
+			
 			double valorChileno = Double.parseDouble(valor);
-			final double reais = 0.0057;
+			
+			double reais = 0.0057;
+			
 			double valorConvertido = valorChileno * reais;
+			
 			msg = "O valor em Reais é: " + valorConvertido;
+			
 			JOptionPane.showMessageDialog(null, msg);
+			
 			continuar();
 		}
 	}
